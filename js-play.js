e5eb4683b4b3ff49c0be2592c20e914ae98ec767
Amanda Baker <mandykatebaker@Amandas-MacBook-Pro.local>
2016-07-16 21:22:42 +1200

    fixed js

--- js-play.js
+++ js-play.js
@@ -1,35 +1,26 @@
 var myImages = new Array();
 
-myImages[1] = "billMurray-1.jpg";
-myImages[2] = "billMurray-2.jpg";
+myImages[0] = "billMurray-1.jpg";
+myImages[1] = "billMurray-2.jpg";
 
 var colors = ["rgb(44,62,80)", "rgb(243,156,18)"]
 
-//function generateImage (text, author) {
-  //var newQuote = {};
-  //newQuote.text = text;
-  //newQuote.author = author;
-  //quoteArray.push(newQuote);
-//}
-
-
-function generateImage(){
+ function generateImage(){
   var random = getRandom(myImages.length);
   var randomColor = getRandom(colors.length);
-  console.log(myImages[random]);
-  document.getElementById("random-images").innerHTML = myImages[random];
+  document.getElementById("random-images").src = myImages[random];
   document.body.style.backgroundColor = colors[randomColor];
 
-  //document.getElementById('tweet').innerHTML = ""
-
-  // twttr.widgets.createShareButton(
-  //   '',
-  //   document.getElementById('tweet'),
-  //   {
-  //     text: allQuotes[random].text
-  //   }
-  // );
 }
+
+
+//function generateImage(){
+  //var random = getRandom(myImages.length);
+  //var randomColor = getRandom(colors.length);
+  //console.log(myImages[random]);
+  //document.getElementById("random-images").innerHTML = myImages[random];
+  //document.body.style.backgroundColor = colors[randomColor];
+//}
 
 function getRandom(length) {
   return Math.floor(Math.random() * (length -1 - 0 + 1)) + 0;
@@ -37,18 +28,11 @@ function getRandom(length) {
 
 var random = getRandom(myImages.length);
 
-//twttr.widgets.createShareButton(
-  //'',
-  //document.getElementById('tweet'),
-  //{
-    //text: allQuotes[random].text
-  //}
-//);
 
 
-// generateQuote("random string", "authortest");
+//document.getElementById("random-images").src = myImages[random];
 
-// console.log(allQuotes);
 
-//document.getElementById("random-images").innerHTML = myImages[random];
-document.write('<img src="'+myImages[rnd]+'" border="0">');
+//document.getElementById('random-images').innerHTML="<img src='images/billMurray-1.jpg' border=0/></a>";
+
+//document.getElement('<img src="'+myImages[rnd]+'" border="0">');
