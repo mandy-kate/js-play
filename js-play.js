e0d9f64d7ea02273251e67d521de34652f313fbe
Amanda Baker <mandykatebaker@Amandas-MacBook-Pro.local>
2016-07-16 21:59:21 +1200

    added jpegs and colors

--- js-play.js
+++ js-play.js
@@ -2,8 +2,12 @@ var myImages = new Array();
 
 myImages[0] = "billMurray-1.jpg";
 myImages[1] = "billMurray-2.jpg";
+myImages[2] = "billMurray-3.jpg";
+myImages[3] = "billMurray-4.jpg";
+myImages[4] = "billMurray-5.jpg";
+myImages[5] = "billMurray-6.jpg";
 
-var colors = ["rgb(44,62,80)", "rgb(243,156,18)"]
+var colors = ["rgb(44,62,80)", "rgb(81,85,35)", "rgb(35,55,49)", "rgb(55,35,40)", "rgb(85,39,35)", "rgb(35,81,85)"]
 
  function generateImage(){
   var random = getRandom(myImages.length);
@@ -30,7 +34,7 @@ var random = getRandom(myImages.length);
 
 
 
-//document.getElementById("random-images").src = myImages[random];
+document.getElementById("random-images").src = myImages[random];
 
 
 //document.getElementById('random-images').innerHTML="<img src='images/billMurray-1.jpg' border=0/></a>";
